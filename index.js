1d0fe0db2537e3cf3f7d74f94a47f17a6c176c20
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-11 23:44:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+  let matches = []
+  for (let i = 0; i < drivers.length; i++) {
+    if (drivers[i].toLowerCase() === name.toLowerCase()) matches.push(drivers[i])
+  }
+  return matches;
+};
+
+
+function fuzzyMatch(drivers, string) {
+  let matches = drivers.filter(n => {
+       n.substring(0,2) === string;
+  });
+  return matches;
+};
+
+function matchName(drivers, string) {
+  let matches = drivers.filter(n => {
+      n.name === string;
+      console.log(matches)
+  });
+    return matches;
+};
